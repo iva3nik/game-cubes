28c138fbd9e8af87ef2ed8f8d63227880b3dacb5
Ivan Nikitin <nikitin.ivan@breezzor.ru>
2021-12-12 22:00:51 +0300

    feat: make function renderBox

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const $start = document.querySelector('#start')
 const $game = document.querySelector('#game')
 
+let score = 0
+
 $start.addEventListener('click', startGame)
+$game.addEventListener('click', hadnleBoxClock)
 
 function startGame() {
   $game.style.backgroundColor = '#fff'
@@ -9,6 +12,24 @@ function startGame() {
   renderBox()
 }
 
-function renderBox() {
+function hadnleBoxClock(event) {
+  if (event.target.dataset.box) {
+    score++
+    renderBox()
+  }
+}
 
+function renderBox() {
+  $game.innerHTML = ''
+  const box = document.createElement('div')
+
+  box.style.height = box.style.width = '50px'
+  box.style.position = 'absolute'
+  box.style.backgroundColor = '#000'
+  box.style.top = '50px'
+  box.style.left = '50px'
+  box.style.cursor = 'pointer'
+  box.setAttribute('data-box', 'true')
+
+  $game.insertAdjacentElement('afterbegin', box)
 }
